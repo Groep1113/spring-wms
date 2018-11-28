6c9667a98ce49dd7ed7602b96784de59e516db4a
Wouter Houtsma <wouter@DESKTOP-A9H7V4M.localdomain>
2018-11-28 15:53:17 +0100

    Met hibernate nu, en een beetje structuur

--- src/main/java/base/Application.java
+++ src/main/java/base/Application.java
@@ -2,12 +2,15 @@ package base;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.domain.EntityScan;
 import org.springframework.context.annotation.ComponentScan;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
 @SpringBootApplication
-@ComponentScan(basePackages = {"base", "interfaces"})
+@EnableJpaRepositories("repository")
+@ComponentScan(basePackages = {"base", "entity", "controller", "repository"})
+@EntityScan("entity")
 public class Application {
-
     public static void main(String[] args) {
         System.out.println("Hello world!");
         SpringApplication.run(Application.class, args);
