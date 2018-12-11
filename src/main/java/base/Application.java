007e6bb1142e9d8bf67bf1cabdade0a4fe97c526
Peter Schriever <peterzen72@gmail.com>
2018-12-11 17:53:59 +0100

    bug fixes to make react frontend work

--- src/main/java/base/Application.java
+++ src/main/java/base/Application.java
@@ -3,8 +3,12 @@ package base;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.domain.EntityScan;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
 
 @SpringBootApplication
 @EnableJpaRepositories("repository")
@@ -15,4 +19,15 @@ public class Application {
         System.out.println("Hello world!");
         SpringApplication.run(Application.class, args);
     }
+
+    @Bean
+    public WebMvcConfigurer corsConfigurer() {
+        return new WebMvcConfigurer() {
+            @Override
+            public void addCorsMappings(CorsRegistry registry) {
+                registry.addMapping("/**").allowedOrigins("*");
+            }
+        };
+    }
+
 }
