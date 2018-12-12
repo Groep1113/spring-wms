8b0d17b451dd06660a1d204ddde848b77a92fd3f
Wesley <wesley.westerhof@gmail.com>
2018-12-12 20:05:02 +0100

    styling fixes

--- src/main/java/graphql/Mutation.java
+++ src/main/java/graphql/Mutation.java
@@ -12,8 +12,6 @@ import org.springframework.stereotype.Component;
 import repository.ItemRepository;
 import repository.LocationRepository;
 import repository.UserRepository;
-
-import java.util.HashSet;
 import java.util.Set;
 
 @Component
@@ -32,6 +30,10 @@ public class Mutation implements GraphQLMutationResolver {
         this.locationRepository = locationRepository;
     }
 
+    private String idNotFoundMessage(int id, String entity){
+        return "No " + entity.toLowerCase() + " found with id " + id + ".";
+    }
+
     public LoginPayload login(String email, String password) throws GraphQLException {
         User user = userRepository
             .authenticate(email, password)
@@ -45,52 +47,56 @@ public class Mutation implements GraphQLMutationResolver {
         return new LoginPayload(token, user);
     }
 
-    public Item createItem(String name, String code, int recommended_stock, int locationId, DataFetchingEnvironment env) throws GraphQLException {
+    public Item createItem(String name, String code, int recommendedStock, int locationId, DataFetchingEnvironment env) {
         AuthContext.requireAuth(env);
 
-        return itemRepository
-                .save(new Item(
+        return itemRepository.save(new Item(
                         name,
                         code,
-                        recommended_stock,
+                        recommendedStock,
                         locationRepository
                             .findById(locationId)
-                            .orElseThrow(() -> new GraphQLException("No location found with id " + locationId + "."))));
+                            .orElseThrow(() -> new GraphQLException(idNotFoundMessage(locationId, Location.class.getSimpleName())))));
     }
 
-    public Location createLocation(String code, int depth, int width, int height, DataFetchingEnvironment env) throws GraphQLException {
+    public Location createLocation(String code, int depth, int width, int height, DataFetchingEnvironment env) {
         AuthContext.requireAuth(env);
         
         return locationRepository.save(new Location(code, depth, width, height));
     }
 
-    // TODO: pas de repository aan zodat deze code wat netter wordt.
-    // TODO: pas de repository aan zodat de setters ook many-to-many zijn
-    public Item itemChangeLocation(int itemId, int locationId, DataFetchingEnvironment env) throws GraphQLException {
+    public Item itemChangeLocation(int itemId, int locationId, DataFetchingEnvironment env) {
         AuthContext.requireAuth(env);
 
-        Location location = locationRepository.findById(locationId).orElseThrow(() -> new GraphQLException("No location found with id " + locationId + "."));
-        Set<Location> locations = new HashSet<>();
+        Item item = itemRepository
+                .findById(itemId)
+                .orElseThrow(() -> new GraphQLException(idNotFoundMessage(itemId, Item.class.getSimpleName())));
+        Set<Location> locations = item.getLocations();
+
+        Location location = locationRepository
+                .findById(locationId)
+                .orElseThrow(() -> new GraphQLException(idNotFoundMessage(locationId, Location.class.getSimpleName())));
+
         locations.add(location);
-        Item item = itemRepository.findById(itemId).orElseThrow(() -> new GraphQLException("No item found with id " + itemId + "."));
-        item.setLocations(locations);
         itemRepository.save(item);
         return item;
     }
 
-    // TODO: pas repository aan zodat boolean terug wordt gegeven.
-    public Boolean deleteItem(int id, DataFetchingEnvironment env) throws GraphQLException {
+    public Boolean deleteItem(int id, DataFetchingEnvironment env) {
         AuthContext.requireAuth(env);
 
-        itemRepository.delete(itemRepository.findById(id).orElseThrow(() -> new GraphQLException("No item found with id " + id + ".")));
+        itemRepository.delete(itemRepository
+                .findById(id)
+                .orElseThrow(() -> new GraphQLException(idNotFoundMessage(id, Item.class.getSimpleName()))));
         return true;
     }
 
-    // TODO: pas repository aan zodat boolean terug wordt gegeven.
-    public Boolean deleteLocation(int id, DataFetchingEnvironment env) throws GraphQLException {
+    public Boolean deleteLocation(int id, DataFetchingEnvironment env) {
         AuthContext.requireAuth(env);
 
-        locationRepository.delete(locationRepository.findById(id).orElseThrow(() -> new GraphQLException("No location found with id " + id + ".")));
+        locationRepository.delete(locationRepository
+                .findById(id)
+                .orElseThrow(() -> new GraphQLException(idNotFoundMessage(id, Location.class.getSimpleName()))));
         return true;
     }
 }
