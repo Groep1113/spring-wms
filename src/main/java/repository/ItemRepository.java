b19218abb38f3251fe390bb734c885375141fb84
Wesley <wesley.westerhof@gmail.com>
2018-12-14 18:05:50 +0100

    Redundant methods removed from repositories

--- src/main/java/repository/ItemRepository.java
+++ src/main/java/repository/ItemRepository.java
@@ -5,13 +5,4 @@ import org.springframework.data.repository.CrudRepository;
 
 public interface ItemRepository extends CrudRepository<Item, Integer> {
 
-
-    //create
-    <S extends Item> S save(S entity);
-
-    //readAll
-    Iterable<Item> findAll();
-
-    //delete
-    void delete(Item entity);
 }
