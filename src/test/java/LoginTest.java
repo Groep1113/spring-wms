aaeadf6c3f2f9841089bedb0d70e514a8d50a8fe
Peter Schriever <peterzen72@gmail.com>
2019-01-09 12:33:21 +0100

    begin test code, folders ed

---
+++ src/test/java/LoginTest.java
@@ -0,0 +1,18 @@
+import graphql.Mutation;
+import mocks.MockUserRepo;
+import org.junit.Before;
+import org.junit.Test;
+
+public class LoginTest {
+
+    @Before
+    public void setup() {
+        this.mutationObj = new Mutation(new MockUserRepo(), )
+    }
+
+    @Test
+    public void testIets() {
+
+    }
+
+}
