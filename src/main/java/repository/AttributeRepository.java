ce6f4fe3c177a319743ad340bdd4af9a3e865dc8
Wesley <wesley.westerhof@gmail.com>
2019-01-11 22:21:56 +0100

    rename to Attribute

---
+++ src/main/java/repository/AttributeRepository.java
@@ -0,0 +1,7 @@
+package repository;
+
+import entity.Attribute;
+import org.springframework.data.repository.CrudRepository;
+
+public interface AttributeRepository extends CrudRepository<Attribute, Integer> {
+}
