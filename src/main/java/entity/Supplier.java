1033296f45646b72fad0f71549cf4cbf3e995556
Wesley <wesley.westerhof@gmail.com>
2019-01-17 11:13:32 +0100

    Changed primary key generation strategy to identity for all entities

--- src/main/java/entity/Supplier.java
+++ src/main/java/entity/Supplier.java
@@ -8,7 +8,7 @@ import java.util.Set;
 public class Supplier {
     // fields
     @Id
-    @GeneratedValue(strategy = GenerationType.AUTO)
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Integer id;
 
     private String name;
