47fc6ef00b738c1e00aea45af86113f63a476e3b
Peter Schriever <peterzen72@gmail.com>
2019-01-18 12:04:44 +0100

    start of item tests

--- src/test/java/mocks/LocationRepo.java
+++ src/test/java/mocks/LocationRepo.java
@@ -1,11 +1,27 @@
 package mocks;
 
+import entity.Account;
 import entity.Location;
 import repository.LocationRepository;
 
+import java.util.HashMap;
+import java.util.HashSet;
 import java.util.Optional;
 
 public class LocationRepo implements LocationRepository {
+    private HashMap<Integer, Location> locationStore = new HashMap<>();
+
+    public LocationRepo() {
+        Location loc = new Location();
+//        loc.setAccount();
+        loc.setCode("UT1");
+        loc.setDepth(0);
+        loc.setWidth(0);
+        loc.setHeight(0);
+        loc.setId(1);
+        locationStore.put(1, loc);
+    }
+
     @Override
     public <S extends Location> S save(S entity) {
         return null;
@@ -17,7 +33,8 @@ public class LocationRepo implements LocationRepository {
     }
 
     @Override
-    public Optional<Location> findById(Integer integer) {
+    public Optional<Location> findById(Integer id) {
+        if (locationStore.containsKey(id)) return Optional.of(locationStore.get(id));
         return Optional.empty();
     }
 
