f12f51213a89ab22f541df7d0ee704d58152556c
Wouter Houtsma <wouterhoutsma@gmail.com>
2019-01-19 17:57:08 +0100

    Fixed a bug in UserSeeder

--- src/main/java/seeder/UserSeeder.java
+++ src/main/java/seeder/UserSeeder.java
@@ -11,15 +11,16 @@ public class UserSeeder extends TableSeeder{
         super(repository, "user");
     }
 
+    @Override
     public void seedJob() {
         String[] firstNames = {"Bob", "Henk", "Klaas", "Piet", "Froukje", "Linda"};
         String[] surnames = {"de Vries", "Klaassen", "Teunissen", "Boxma", "de Wilg", "Vredeveld"};
         Random rand = new Random();
         for(int i = 0; i < 5; i++) {
             User u = new User();
-            String firstName = firstNames[rand.nextInt(firstNames.length - 1)];
+            String firstName = firstNames[rand.nextInt(firstNames.length)];
             u.setFirstName(firstName);
-            u.setLastName(surnames[rand.nextInt(surnames.length - 1)]);
+            u.setLastName(surnames[rand.nextInt(surnames.length)]);
             u.setEmail(firstName.toLowerCase()   + "@bs-htg.com");
             u.setPassword("password");
             this.repository.save(u);
