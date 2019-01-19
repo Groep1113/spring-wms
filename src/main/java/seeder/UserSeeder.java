1b05f6d41eca2a48f36e8d8780c094d90c5e2a45
Wouter Houtsma <wouterhoutsma@gmail.com>
2019-01-19 14:51:24 +0100

    Added UserSeeder

--- src/main/java/seeder/UserSeeder.java
+++ src/main/java/seeder/UserSeeder.java
@@ -1,4 +1,29 @@
 package seeder;
 
+import entity.User;
+import org.springframework.data.repository.CrudRepository;
+
+import java.util.Random;
+
 public class UserSeeder {
+    private CrudRepository repository;
+    public UserSeeder(CrudRepository repository) {
+        this.repository = repository;
+    }
+
+    public void seed() {
+        String[] firstNames = {"Bob", "Henk", "Klaas", "Piet", "Froukje", "Linda"};
+        String[] surnames = {"de Vries", "Klaassen", "Teunissen", "Boxma", "de Wilg", "Vredeveld"};
+        Random rand = new Random();
+        for(int i = 0; i < 5; i++) {
+            User u = new User();
+            String firstName = firstNames[rand.nextInt(firstNames.length - 1)];
+            u.setFirstName(firstName);
+            u.setLastName(surnames[rand.nextInt(surnames.length - 1)]);
+            u.setEmail(firstName + "@bs-htg.com");
+            u.setPassword(Integer.toString(rand.nextInt(10) + 1));
+            this.repository.save(u);
+        }
+
+    }
 }
