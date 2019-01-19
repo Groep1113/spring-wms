504d250d47b384ef7c70a2a4153c8d65f0ed934f
Wouter Houtsma <wouterhoutsma@gmail.com>
2019-01-19 17:58:49 +0100

    Fixed imports on LocationSeeder

--- src/main/java/seeder/LocationSeeder.java
+++ src/main/java/seeder/LocationSeeder.java
@@ -1,14 +1,7 @@
 package seeder;
 
-import entity.Category;
 import entity.Location;
 import org.springframework.data.repository.CrudRepository;
-import repository.CategoryRepository;
-
-import java.util.ArrayList;
-import java.util.List;
-import java.util.Random;
-import java.util.Set;
 
 public class LocationSeeder extends TableSeeder{
 
